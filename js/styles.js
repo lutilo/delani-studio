ff81e64927fb75821630c00d14ea6a29abfe5eaa
brayooh <brianndichu.bn@gmail.com>
2020-05-23 12:33:14 +0300

    creates function for toggle effect

--- js/styles.js
+++ js/styles.js
@@ -5,26 +5,3 @@ $('.main').click(function () {
 });
 
 
-$(".portfolio img").hover(function (e) {
-    $(this).animate({ opacity: .5 })
-}, function () {
-    $(this).stop().animate({ opacity: 1 })
-})
-$("button").click(function () {
-    var client = document.getElementById("name").value;
-    $('#form').submit(function (event) {
-        var display1 = $("input:first").val();
-        var display2 = $("input#email").val();
-        if (display1 && display2 !== "") {
-            alert('dear ' + client + ', we have received your message. ' + 'Thank you for your feedback.');
-        }
-        else {
-            alert("invalid input");
-        }
-        event.preventDefault();
-    });
-    $('input #name').css('color', 'white');
-    $('input #email').css('color', 'white');
-    $('input #message').css('color', 'white');
-  }
-)
