dac2a06ec8fce65443cd2e8a691362450c8f5196
brayooh <brianndichu.bn@gmail.com>
2020-05-23 12:34:15 +0300

    adds alert message

--- js/styles.js
+++ js/styles.js
@@ -10,3 +10,21 @@ $(".portfolio img").hover(function (e) {
 }, function () {
     $(this).stop().animate({ opacity: 1 })
 })
+$("button").click(function () {
+    var client = document.getElementById("name").value;
+    $('#form').submit(function (event) {
+        var display1 = $("input:first").val();
+        var display2 = $("input#email").val();
+        if (display1 && display2 !== "") {
+            alert('dear ' + client + ', we have received your message. ' + 'Thank you for your feedback.');
+        }
+        else {
+            alert("invalid input");
+        }
+        event.preventDefault();
+    });
+    $('input #name').css('color', 'white');
+    $('input #email').css('color', 'white');
+    $('input #message').css('color', 'white');
+  }
+)
